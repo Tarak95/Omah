d17c264d70a7b49afabe7f21bb441964586106f7
Tarak <tarek8295@gmail.com>
2026-03-02 19:13:17 +0600

    New update added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,14 +7,14 @@ const Navbar = () => {
         <div className='container'>
             <div className='box'>
                 <div className='logo'>
-                    <img src='images/logo.png'/>
+                    <img src='images/logo.png' alt='Company Logo'/>
                 </div>
                 <div className='nav_item'>
                     <ul>
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>About</a></li>
-                        <li><a href='#'>Properties</a><i class="fa-solid fa-angle-down"></i></li>
-                        <li><a href='#'>Contact</a></li>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/about'>About</a></li>
+                        <li><a href='/properties'>Properties</a><i className="fa-solid fa-angle-down"></i></li>
+                        <li><a href='/contact'>Contact</a></li>
                     </ul>
                 </div>
             </div>
